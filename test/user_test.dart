6db64b458ffd59d594d95eea3c4051e4ae05c59d
dna2121 <dnatayawibisono@gmail.com>
2023-10-17 14:11:56 +0700

    Custom User Test #test-fails

--- test/user_test.dart
+++ test/user_test.dart
@@ -18,4 +18,26 @@ void main() {
       expect(user.name, equals('No Name'));
     });
   });
+
+  /* Test ke - dua */
+  group('Custom User Test', () {
+    User user1 = User(id: 1);
+    User user2 = User(name: 'Denise');
+    User user3 = User(id: 1, name: 'Denise');
+
+    test("Single parameter (id) is valid.", () {
+      expect(user1.id, equals(1));
+      expect(user1.name, equals('No Name'));
+    });
+
+    test("Single parameter (name) is valid.", () {
+      expect(user2.id, equals(0));
+      expect(user2.name, equals('Denise'));
+    });
+
+    test("All parameters are valid.", () {
+      expect(user3.id, equals(1));
+      expect(user3.name, equals('Denise'));
+    });
+  });
 }
